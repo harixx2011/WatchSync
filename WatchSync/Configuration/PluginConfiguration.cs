204c9edd4697296586e34abce1a41a9dc0aa60b8
cyy3648 <harm@nesdroht.de>
2026-09-08 16:16:20 +0200

    feat: Add config.html page

--- WatchSync/Configuration/PluginConfiguration.cs
+++ WatchSync/Configuration/PluginConfiguration.cs
@@ -2,7 +2,10 @@ using MediaBrowser.Model.Plugins;
 
 namespace WatchSync.Configuration;
 
-class PluginConfiguration : BasePluginConfiguration
+public class PluginConfiguration : BasePluginConfiguration
 {
-
+    public string Options { get; set; } = "OneOption";
+    public int AnInteger { get; set; }
+    public bool TrueFalseSetting { get; set; }
+    public string AString { get; set; } = string.Empty;
 }
--- WatchSync/Plugin.cs
+++ WatchSync/Plugin.cs
@@ -1,12 +1,26 @@
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
 using WatchSync.Configuration;
 
 namespace WatchSync;
 
-class Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : BasePlugin<PluginConfiguration>(applicationPaths, xmlSerializer)
+public class Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : BasePlugin<PluginConfiguration>(applicationPaths, xmlSerializer), IHasWebPages
 {
     public override string Name => "WatchSync";
     public override string Description => "Sync watch state between users.";
+    public override Guid Id => Guid.Parse("c035a825-eb06-49b4-a962-3703d4acb177");
+
+    public IEnumerable<PluginPageInfo> GetPages()
+    {
+        return [
+            new PluginPageInfo {
+                Name = Name,
+                DisplayName = "WatchSync Config",
+                EnableInMainMenu = true,
+                EmbeddedResourcePath = "WatchSync.Configuration.configPage.html"
+            }
+        ];
+    }
 }
